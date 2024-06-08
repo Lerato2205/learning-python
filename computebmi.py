29affaa0b9d74ad9b6dab5f74d7307d88f5d3863
Lerato2205 <137944941+Lerato2205@users.noreply.github.com>
2024-06-08 15:34:40 +0200

    chapter4 of intro to python covered

---
+++ computebmi.py
@@ -0,0 +1,16 @@
+weight = eval(input("Enter weight in pounds: ")) 
+height = eval(input("Enter height in inches: "))
+KILOGRAMS_PER_POUND = 0.45359237
+METERS_PER_INCH = 0.0254	
+weightInKilograms = weight * KILOGRAMS_PER_POUND
+heightInMeters = height * METERS_PER_INCH
+bmi = weightInKilograms / (heightInMeters * heightInMeters) 
+print("BMI is", format(bmi, ".2f"))
+if bmi < 18.5:
+ print("Underweight")
+elif bmi < 25:
+ print("Normal")
+elif bmi < 30:
+ print("Overweight")
+else:
+ print("Obese")
